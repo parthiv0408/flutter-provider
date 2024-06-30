abb81be88701da82872cdce78dd9d57f71bda6e2
Parthiv <parthivchovatiya0408@gmail.com>
2024-06-30 11:53:48 +0530

    Theme Add

--- lib/todo/model/todo_model.dart
+++ lib/todo/model/todo_model.dart
@@ -2,5 +2,8 @@ class Todo {
   String title;
   bool isCompleted;
 
-  Todo({required this.title, this.isCompleted = false});
+  Todo({
+    required this.title,
+    this.isCompleted = false,
+  });
 }
